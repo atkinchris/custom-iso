6138d60b9a1010f2b20bc8c4ff94e75c25e6420c
Chris Atkin <atkinchris@gmail.com>
2019-11-24 08:23:05 +0000

    Update Docker setup for new config

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ function on_exit {
 trap on_exit EXIT
 
 docker-compose kill
-docker-compose rm -vf
+# docker-compose rm -vf
 docker-compose build
 docker-compose up --remove-orphans --abort-on-container-exit
